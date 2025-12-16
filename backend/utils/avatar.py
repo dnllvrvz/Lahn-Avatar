3e05f68d0c0184096fa2e04d335c85680de96d8b
Mayowa Osibodu <osibodumayowa@gmail.com>
2025-12-15 22:49:00 -0800

    MultiAvatar Update

--- backend/utils/avatar.py
+++ backend/utils/avatar.py
@@ -63,16 +63,17 @@ def download_drive_folder(folder_id, output_dir="./data"):
     subprocess.run(cmd, shell=True)
 
 
-def fetch_system_prompt_from_gdoc():
-    print(' Updating system prompt...')
-    url = "https://docs.google.com/document/d/1NYOOy8KkaLDBwvHvEVg1hVDY5yvHeLACUpCEkJVM8Kw/export?format=txt"
-    response = requests.get(url)
+def fetch_system_prompt_from_gdoc(avatar_id='0', system_prompt_url="https://docs.google.com/document/d/1NYOOy8KkaLDBwvHvEVg1hVDY5yvHeLACUpCEkJVM8Kw/export?format=txt" ):
+    print(' Updating system prompt for avatar: ' + avatar_id + '...')
+    prompt_dir = 'avatars_context/' + avatar_id+'/prompt'
+    # url = "https://docs.google.com/document/d/1NYOOy8KkaLDBwvHvEVg1hVDY5yvHeLACUpCEkJVM8Kw/export?format=txt"
+    response = requests.get(system_prompt)
     response.raise_for_status()
     prompt = response.text.strip()
     # prompt = prompt[:prompt.find('General Internal Impressions')]
 
-    base_dir = os.path.dirname(os.path.abspath(__file__))
-    file_path = os.path.join(base_dir, 'system_prompt.txt')
+    # base_dir = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(prompt_dir, 'system_prompt.txt')
     with open(file_path, 'w') as f:
         f.write(prompt)
     print(' Done.')

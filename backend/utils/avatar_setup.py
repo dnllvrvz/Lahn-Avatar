6d79bc029263392cdda76f7abf9c5672b1a4488a
Mayowa Osibodu <osibodumayowa@gmail.com>
2025-12-18 02:01:45 -0800

    Updating codebase to scalably support multiple avatars

--- backend/utils/avatar_setup.py
+++ backend/utils/avatar_setup.py
@@ -8,7 +8,7 @@ llm_choice = 'gpt-4' #'mistral-large-instruct' #"gemma-3-27b-it"
 
 
 # In-memory storage (replace with DB later if you like)
-avatars_path = '../avatars.json'
+avatars_path = 'avatars.json'
 avatars = json.load(open(avatars_path, 'r'))
 
 avatar_llms = {}
